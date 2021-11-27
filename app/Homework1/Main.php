fb6e152b1689556d494f216fb34e3a6983676d31
felikidze <shtol.art@yandex.ru>
2021-11-27 11:41:40 +0500

    extra removed

--- app/Homework1/Main.php
+++ app/Homework1/Main.php
@@ -4,12 +4,10 @@ require_once 'Basket.php';
 require_once 'Order.php';
 require_once 'BasketPosition.php';
 require_once 'Product.php';
-
 //$obj = new Basket([new Product('twix', 60)]);
 //данная реализация предусматривает, что если у нас в корзине уже существует продукт с определенной ценой,
 //то новый продукт с таким же именем не может иметь другую цену, что звучит вполне логично ;)
-$oneProduct = new Product('twix', 60); // создаем продукт
-$oneBasketPosition = new BasketPosition($oneProduct, 2); // добавляем позицию для будующей корзины с опр. кол-вом
+//$oneProduct = new Product('twix', 60); // создаем продукт
 $basket = new Basket(); // инициализируем корзину
 $basket->addProduct(new Product('twix', 60), 3);
 $basket->addProduct(new Product('twix', 60), 2); //добавляем элемент в корзину с таким же именем для теста объединения
